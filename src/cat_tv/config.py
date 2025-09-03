8f6ac00e48e30f4b1e67eea9a6917e3072a92879
Jeff Crouse <jeff@seethroughlab.com>
2025-09-03 12:29:06 -0400

    changed port

--- src/cat_tv/config.py
+++ src/cat_tv/config.py
@@ -20,7 +20,7 @@ class Config:
     # Flask settings
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key-change-in-production")
     FLASK_HOST = os.getenv("FLASK_HOST", "0.0.0.0")
-    FLASK_PORT = int(os.getenv("FLASK_PORT", "5000"))
+    FLASK_PORT = int(os.getenv("FLASK_PORT", "8080"))
     DEBUG = os.getenv("DEBUG", "False").lower() == "true"
     
     # YouTube settings
